eea07b53b5c7ca1d055b531157edb0677271aa1e
raisongran@protonmail.com <doCzQ4Ue20qPNPjNkuaozGe8bYk0qZFBfbEJUNIhz66ADMFT5KnWBl7wD7psc98o8FDCNCSbk4bBTRnG4nfauFTm6dLpFMygbd6Q>
2023-02-17 21:41:46 +0200

    staging

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,6 +3,12 @@ SITENAME = 'kskkaskad'
 SITEURL = ''
 
 PATH = 'content'
+ARTICLE_SAVE_AS = '{category}/{slug}.html'
+ARTICLE_URL = '{category}/{slug}.html'
+DEFAULT_PAGINATION = False
+DIRECT_TEMPLATES = ['index']
+TAGS_SAVE_AS = ''
+TAG_SAVE_AS = ''
 
 TIMEZONE = 'Europe/Rome'
 
